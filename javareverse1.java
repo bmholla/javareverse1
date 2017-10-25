4f451f9aea3979514a8d4de5f4e5b56845925267
Brian Holland <bholland@majordomo.local>
2017-10-25 16:15:55 -0600

    version 1 implemented

--- javareverse1.java
+++ javareverse1.java
@@ -1,14 +1,52 @@
+import java.util.List;
+import java.util.LinkedList;
+
 class javareverse1
 {
-    List l;
+    //List<E> l;
 
-    public javareverse1(List l)
+    /*
+    public javareverse1(List<E> l)
     {
         this.l = l;
     }
+    */
 
-    public List reverse(List l)
+    public static <E> List<E> reverse(List<E> l)
     {
-        
+        if(l.size() > 1)
+        {
+            E e = l.get(0);
+            List<E> ll = reverse(l.subList(1, l.size()));
+            //ll.add(0, e);
+            ll.add(e);
+            return ll;  
+        }
+        return l;
+    }
+
+    public static void main(String[] args)
+    {
+        List<Integer> li = CreateIntList.createIntList(10);
+
+        for(Integer i : li)
+            System.out.println(i);
+        System.out.println("");
+    
+        List<Integer> li2 = reverse(li);
+
+        for(Integer i : li2)
+            System.out.println(i);
+    }
+}
+
+class CreateIntList
+{
+    public static LinkedList<Integer> createIntList(int count)
+    {
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+        for(int i=0;i<count;i++)
+            ll.add(i);    
+        return ll;
     }
 }
