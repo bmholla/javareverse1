cbc21bde1883e0645c6c5012b93fba47729e0fdc
Brian Holland <bholland@majordomo.local>
2017-10-25 15:41:20 -0600

    initial

---
+++ javareverse1.java
@@ -0,0 +1,14 @@
+class javareverse1
+{
+    List l;
+
+    public javareverse1(List l)
+    {
+        this.l = l;
+    }
+
+    public List reverse(List l)
+    {
+        
+    }
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+makejavareverse1: javareverse1.java
+	javac javareverse1.java
+
+.PHONY: clean
+
+clean:
+	rm -f *.class
